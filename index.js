796fa20dc941348342256da054cc2d68a98ad5b1
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 18:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(arr, cVal, sVal) {
   console.log("start")
-  for (let i = 0; i < arr.size; i++) {
+  for (var i = 0; i < arr.size; i++) {
     if (arr[i] === sVal) {
       console.log("Found stop value")
       break;
