670a8a7c061785decb278bf5e0d5c19cb5a7440f
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 18:18:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function breakOut(arr, cVal, sVal) {
+  arr.forEach(elem => {
+    if (elem === sVal) {
+      break;
+    }
+
+    elem = cVal
+  })
+}
+
+function keepGoing(arr, cVal, sVal) {
+  
+}
+
+function findBy(arr, findFn)
